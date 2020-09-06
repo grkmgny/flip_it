277e99463f2356f10064501de0d16ad71f220574
Gorkem <gorkem.gunayy@gmail.com>
2020-09-06 11:00:21 +0300

    Commit

---
+++ FlipIt_Oyunu/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace FlipIt_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbVertical = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbHorizontal = new System.Windows.Forms.ComboBox();
+            this.txtisim = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpGame = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(7, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Moves:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbVertical);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbHorizontal);
+            this.groupBox1.Controls.Add(this.txtisim);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblScore);
+            this.groupBox1.Controls.Add(this.lblMoves);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(628, 143);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Start";
+            // 
+            // cmbVertical
+            // 
+            this.cmbVertical.FormattingEnabled = true;
+            this.cmbVertical.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmbVertical.Location = new System.Drawing.Point(271, 102);
+            this.cmbVertical.Name = "cmbVertical";
+            this.cmbVertical.Size = new System.Drawing.Size(185, 28);
+            this.cmbVertical.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(157, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Vertical:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(494, 26);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(111, 61);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(157, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Horizontal:";
+            // 
+            // cmbHorizontal
+            // 
+            this.cmbHorizontal.FormattingEnabled = true;
+            this.cmbHorizontal.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmbHorizontal.Location = new System.Drawing.Point(271, 66);
+            this.cmbHorizontal.Name = "cmbHorizontal";
+            this.cmbHorizontal.Size = new System.Drawing.Size(185, 28);
+            this.cmbHorizontal.TabIndex = 6;
+            // 
+            // txtisim
+            // 
+            this.txtisim.Location = new System.Drawing.Point(271, 29);
+            this.txtisim.Name = "txtisim";
+            this.txtisim.Size = new System.Drawing.Size(185, 26);
+            this.txtisim.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(157, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Player:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScore.Location = new System.Drawing.Point(72, 69);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(0, 20);
+            this.lblScore.TabIndex = 3;
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMoves.Location = new System.Drawing.Point(72, 29);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(0, 20);
+            this.lblMoves.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(7, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score:";
+            // 
+            // grpGame
+            // 
+            this.grpGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grpGame.Location = new System.Drawing.Point(12, 179);
+            this.grpGame.Name = "grpGame";
+            this.grpGame.Size = new System.Drawing.Size(628, 606);
+            this.grpGame.TabIndex = 2;
+            this.grpGame.TabStop = false;
+            this.grpGame.Text = "Game";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 797);
+            this.Controls.Add(this.grpGame);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbHorizontal;
+        private System.Windows.Forms.TextBox txtisim;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblMoves;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grpGame;
+        private System.Windows.Forms.ComboBox cmbVertical;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FlipIt_Oyunu/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlipIt_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int tiklamasayisi = 0;
+        int puan = 10000;
+        int p1 = 0;
+        int p2 = 0;
+        string isim;
+        private void button1_Click(object sender, EventArgs e)
+        {
+                grpGame.Controls.Clear();
+                lblMoves.Text = "0";
+                lblScore.Text = "10000";
+                tiklamasayisi = 0;
+                puan = 10000;
+                if (txtisim.Text != "")
+                    isim = txtisim.Text;
+                else
+                {
+                    MessageBox.Show("Lütfen bir isim giriniz.");
+                    return;
+
+                }
+                   
+                if (cmbHorizontal.SelectedItem != null && cmbVertical.SelectedItem != null)
+                {
+                    p1 = Convert.ToInt32(cmbHorizontal.SelectedItem);
+                    p2 = Convert.ToInt32(cmbVertical.SelectedItem);
+                    ButonOlustur(p1, p2);
+                }
+                else
+                    MessageBox.Show("Lütfen boyut seçimlerini yapınız");
+                
+      
+        }
+
+        private void DosyayaYaz(string isim, int tiklamasayisi, int puan)
+        {
+
+            
+                string dosyayolu = @"D:\" + isim + ".txt";
+                string[] yazilacakveriler = new string[1];
+                yazilacakveriler[0] = "Ad Soyad:" + isim + "---Hamle Sayısı:" + tiklamasayisi.ToString() + "---Puan:" + puan.ToString();
+                File.WriteAllLines(dosyayolu, yazilacakveriler);
+           
+      
+        }
+
+        private void ButonOlustur(int p1,int p2)
+        {
+            for (int i = 0; i < p2; i++)
+            {
+
+                for (int j = 0; j < p1; j++)
+                {
+                    Button b = new Button();
+                    b.Width = 50;
+                    b.Height = 50;
+                    b.Left = (j * 50) + 128;
+                    b.Top = (i * 50) + 100;
+                    b.BackColor = Color.Black;
+                    b.Click += b_Click;
+                    grpGame.Controls.Add(b); 
+                } 
+            }
+
+        }
+
+        void b_Click(object sender, EventArgs e)
+        {
+            tiklamasayisi++;
+            puan -= 50;
+            lblMoves.Text = tiklamasayisi.ToString();
+            lblScore.Text = puan.ToString();
+
+            Button tiklananbuton = sender as Button;
+            tiklananbuton.BackColor = tiklananbuton.BackColor == Color.Black ? Color.White : Color.Black;
+            Point btnkordinat = tiklananbuton.Location;
+            int Xkordinat = btnkordinat.X;
+            int Ykordinat = btnkordinat.Y;
+            int yenibtnkordinatX = Xkordinat - 50;
+            int yenibtnkordinatY = Ykordinat - 50;
+            int yenibtnkordinatX2 = Xkordinat + 50;
+            int yenibtnkordinatY2 = Ykordinat + 50;
+            int kontrol = 1;
+            int sonuc=0;
+
+            for (int i = 0; i < grpGame.Controls.Count; i++)
+            {
+                if (grpGame.Controls[i] is Button)
+                {
+                    if (grpGame.Controls[i].Location.X == yenibtnkordinatX && grpGame.Controls[i].Location.Y==Ykordinat)                    
+                        grpGame.Controls[i].BackColor = grpGame.Controls[i].BackColor == Color.Black ? Color.White : Color.Black;
+                    
+                    if (grpGame.Controls[i].Location.Y == yenibtnkordinatY && grpGame.Controls[i].Location.X==Xkordinat)                   
+                        grpGame.Controls[i].BackColor = grpGame.Controls[i].BackColor == Color.Black ? Color.White : Color.Black;
+                    
+                    if (grpGame.Controls[i].Location.X == yenibtnkordinatX2 && grpGame.Controls[i].Location.Y == Ykordinat)                    
+                        grpGame.Controls[i].BackColor = grpGame.Controls[i].BackColor == Color.Black ? Color.White : Color.Black;
+                    
+                    if (grpGame.Controls[i].Location.Y == yenibtnkordinatY2 && grpGame.Controls[i].Location.X == Xkordinat)                   
+                        grpGame.Controls[i].BackColor = grpGame.Controls[i].BackColor == Color.Black ? Color.White : Color.Black;
+                    
+                    foreach (Control b in grpGame.Controls)
+                    {
+                        if (b is Button)
+                            sonuc = b.BackColor==Color.White ? kontrol++ : 0;
+                        
+                    }
+                    if (sonuc==grpGame.Controls.Count)
+                    {
+                        MessageBox.Show("Tebrikler!");
+                        foreach (Control b in grpGame.Controls)
+                        {
+                            if (b is Button) b.Enabled = false;
+                        }
+                        btnStart.Text = "Play Again";
+                        DosyayaYaz(isim, tiklamasayisi, puan);
+                        break;
+                    }
+                    else
+                        if(puan==0)
+                        {
+                            MessageBox.Show("Kaybettiniz.");
+                            foreach (Control b in grpGame.Controls)
+                            {
+                                if (b is Button) b.Enabled = false;
+                            }
+                            btnStart.Text="Play Again";
+                            break;
+                        }
+                        else
+                        kontrol = 1;
+                    
+                }
+                 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Flip It!";
+        }
+    }
+}
